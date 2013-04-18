93ce36274a4c8a76b93e9ba1394a715e667f214d
Tim Weninger <weninge1@illinois.edu>
2013-04-18 17:09:37 -0500

    Added ArnetMiner and DBLP dataset drivers

---
+++ src/edu/nd/nina/io/ArnetMiner.java
@@ -0,0 +1,161 @@
+package edu.nd.nina.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import edu.nd.nina.graph.TypedEdge;
+import edu.nd.nina.graph.TypedSimpleGraph;
+import edu.nd.nina.structs.Pair;
+import edu.nd.nina.types.dblp.Author;
+import edu.nd.nina.types.dblp.Paper;
+import edu.nd.nina.types.dblp.Venue;
+import edu.nd.nina.types.dblp.Year;
+
+public class ArnetMiner {
+
+	private static Logger logger = Logger.getLogger(ArnetMiner.class.getName());
+	
+	/**
+	 * . Within this archive, you will find one plain text file. Each line
+	 * begins with an identifier for the data found on that line, as is
+	 * described at the Arnetminer dataset website:
+	 * 
+	 * #* --- paperTitle 
+
+	 * #@ --- Authors
+	 * 
+	 * #year ---- Year
+	 * 
+	 * #conf --- publication venue
+	 * 
+	 * #citation --- number of citations for this paper
+	 * 
+	 * #index ---- index id of this paper
+	 * 
+	 * #% ---- the id of references of this paper (there are multiple lines,
+	 * with each indicating a reference)
+	 * 
+	 * @param dblpGraphFile
+	 */
+	public static void loadDBLPGraphFromFile(InputStream is, TypedSimpleGraph tsg){
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+		String line = "";
+		
+		Map<Integer, Paper> idxToPaper = new TreeMap<Integer, Paper>();
+		List<Pair<Integer, Integer>> refs = new ArrayList<Pair<Integer, Integer>>();
+		
+		Paper paper = null;
+		List<Author> authors = new ArrayList<Author>();
+		Year year = null;
+		Venue v = null;		
+		
+		logger.info("Begin loading ArnetMiner file");
+		
+		int total = 8300000;
+		int perc = -1;
+		int i=0;
+		try {
+			while((line = br.readLine() ) != null){						
+				
+				if(line.trim().isEmpty()){
+					paper = null;
+					authors = new ArrayList<Author>();
+					year = null;
+					v = null;
+					
+					// start fresh
+				}else if(line.startsWith("#*")){
+					
+					if(perc < i++/(float)total * 100f){
+						perc++;
+						logger.info(perc + "% loaded");
+					}
+					
+					paper = new Paper(line.substring(2));
+					
+				}else if(line.startsWith("#@")){
+					String[] as = line.substring(2).split(",");
+					for (String author : as) { 
+						Author a = new Author(author);
+						authors.add(a);						
+					}
+				}else if(line.startsWith("#year")){
+					year = new Year(line.substring(5));					
+				}else if(line.startsWith("#conf")){
+					v = new Venue(line.substring(5));					
+				}else if(line.startsWith("#citation")){
+					paper.setCitations(line.substring(9));
+				}else if(line.startsWith("#index")){
+					paper.setIdx(line.substring(6));
+					tsg.addVertex(paper);
+					for(Author a : authors){
+						tsg.addVertex(a);
+						tsg.addEdge(paper, a);
+					}
+					tsg.addVertex(year);
+					tsg.addEdge(paper, year);
+					tsg.addVertex(v);
+					tsg.addEdge(paper, v);
+					
+					idxToPaper.put(paper.getIdx().hashCode(), paper);
+				}else if(line.startsWith("#%")){
+					// no self citations allowed
+					if(paper.getIdx().equals(line.substring(2))) continue;
+					refs.add(new Pair<Integer, Integer>(paper.getIdx().hashCode(), line.substring(2).hashCode()));
+				}
+			}
+			
+			logger.info("File loaded");
+			
+			logger.info("Begin computing references");
+			
+			total = refs.size();
+			i=0;
+			perc = -1;
+			for(Pair<Integer, Integer> ref : refs){
+				if(i++/(float)total*100f > perc){
+					perc++;
+					logger.info(perc + "% references loaded");
+				}
+				if(idxToPaper.containsKey(ref.p2)){
+					Paper x = idxToPaper.get(ref.p1);
+					Paper y = idxToPaper.get(ref.p2);
+					if(!x.equals(y)){
+						logger.fine("Self reference " + x);
+						tsg.addEdge(x, y);
+					}
+				}
+			}
+			
+			logger.info("Number vertices " + tsg.vertexSet().size());
+			logger.info("Number edges " + tsg.edgeSet().size());
+			logger.info("Graph Loaded");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void main(String[] args){
+		File data = new File("./data/dblp/test.txt");
+		
+		TypedSimpleGraph tsg = new TypedSimpleGraph(TypedEdge.class);
+		try {
+			NINALogger.setup();
+			loadDBLPGraphFromFile(FileHandler.toInputStream(data), tsg);
+			PrintStatistics.PrintTypedGraphStatTable(tsg, "./data/dblp/testStats", "DBLPTypedGraph");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/edu/nd/nina/io/DBLP.java
+++ src/edu/nd/nina/io/DBLP.java
@@ -1,159 +1,180 @@
 package edu.nd.nina.io;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Set;
 import java.util.logging.Logger;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import edu.nd.nina.graph.TypedEdge;
 import edu.nd.nina.graph.TypedSimpleGraph;
-import edu.nd.nina.structs.Pair;
 import edu.nd.nina.types.dblp.Author;
 import edu.nd.nina.types.dblp.Paper;
 import edu.nd.nina.types.dblp.Venue;
-import edu.nd.nina.types.dblp.Year;
 
 public class DBLP {
-
-	private static Logger logger = Logger.getLogger(DBLP.class.getName());
 	
-	/**
-	 * . Within this archive, you will find one plain text file. Each line
-	 * begins with an identifier for the data found on that line, as is
-	 * described at the Arnetminer dataset website:
-	 * 
-	 * #* --- paperTitle 
+	private static Logger logger = Logger.getLogger(DBLP.class.getName());
 
-	 * #@ --- Authors
-	 * 
-	 * #year ---- Year
-	 * 
-	 * #conf --- publication venue
-	 * 
-	 * #citation --- number of citations for this paper
-	 * 
-	 * #index ---- index id of this paper
-	 * 
-	 * #% ---- the id of references of this paper (there are multiple lines,
-	 * with each indicating a reference)
-	 * 
-	 * @param dblpGraphFile
-	 */
-	public static void loadDBLPGraphFromFile(InputStream is, TypedSimpleGraph tsg){
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
-		String line = "";
-		
-		Map<Integer, Paper> idxToPaper = new TreeMap<Integer, Paper>();
-		List<Pair<Integer, Integer>> refs = new ArrayList<Pair<Integer, Integer>>();
-		
-		Paper paper = null;
-		List<Author> authors = new ArrayList<Author>();
-		Year year = null;
-		Venue v = null;		
-		
-		logger.info("Begin loading ArnetMiner file");
-		
-		int total = 8300000;
-		int perc = -1;
-		int i=0;
+	private static void loadDBLPGraphFromFile(InputStream is,
+			TypedSimpleGraph tsg) {
 		try {
-			while((line = br.readLine() ) != null){						
-				
-				if(line.trim().isEmpty()){
-					paper = null;
-					authors = new ArrayList<Author>();
-					year = null;
-					v = null;
-					
-					// start fresh
-				}else if(line.startsWith("#*")){
-					
-					if(perc < i++/(float)total * 100f){
-						perc++;
-						logger.info(perc + "% loaded");
-					}
-					
-					paper = new Paper(line.substring(2));
-					
-				}else if(line.startsWith("#@")){
-					String[] as = line.substring(2).split(",");
-					for (String author : as) { 
-						Author a = new Author(author);
-						authors.add(a);						
-					}
-				}else if(line.startsWith("#year")){
-					year = new Year(line.substring(5));					
-				}else if(line.startsWith("#conf")){
-					v = new Venue(line.substring(5));					
-				}else if(line.startsWith("#citation")){
-					paper.setCitations(line.substring(9));
-				}else if(line.startsWith("#index")){
-					paper.setIdx(line.substring(6));
-					tsg.addVertex(paper);
-					for(Author a : authors){
-						tsg.addVertex(a);
-						tsg.addEdge(paper, a);
-					}
-					tsg.addVertex(year);
-					tsg.addEdge(paper, year);
-					tsg.addVertex(v);
-					tsg.addEdge(paper, v);
-					
-					idxToPaper.put(paper.getIdx().hashCode(), paper);
-				}else if(line.startsWith("#%")){
-					// no self citations allowed
-					if(paper.getIdx().equals(line.substring(2))) continue;
-					refs.add(new Pair<Integer, Integer>(paper.getIdx().hashCode(), line.substring(2).hashCode()));
-				}
-			}
-			
-			logger.info("File loaded");
-			
-			logger.info("Begin computing references");
-			
-			total = refs.size();
-			i=0;
-			perc = -1;
-			for(Pair<Integer, Integer> ref : refs){
-				if(i++/(float)total*100f > perc){
-					perc++;
-					logger.info(perc + "% references loaded");
-				}
-				if(idxToPaper.containsKey(ref.p2)){
-					Paper x = idxToPaper.get(ref.p1);
-					Paper y = idxToPaper.get(ref.p2);
-					if(!x.equals(y)){
-						logger.fine("Self reference " + x);
-						tsg.addEdge(x, y);
-					}
-				}
-			}
-			
-			logger.info("Number vertices " + tsg.vertexSet().size());
-			logger.info("Number edges " + tsg.edgeSet().size());
-			logger.info("Graph Loaded");
+			SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+			SAXParser parser = parserFactory.newSAXParser();
+			ConfigHandler handler = new ConfigHandler(tsg);
+			parser.getXMLReader().setFeature(
+					"http://xml.org/sax/features/validation", true);
+			parser.parse(is, handler);
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Error reading URI: " + e.getMessage());
+		} catch (SAXException e) {
+			System.out.println("Error in parsing: " + e.getMessage());
+		} catch (ParserConfigurationException e) {
+			System.out.println("Error in XML parser configuration: "
+					+ e.getMessage());
 		}
 	}
-	
-	
-	public static void main(String[] args){
-		File data = new File("./data/dblp/test.txt");
+
+	private static class ConfigHandler extends DefaultHandler {
+		TypedSimpleGraph tsg = null;
+		Set<String> paperType = new HashSet<String>();
 		
+		Set<String> venueType = new HashSet<String>();
+
+		public ConfigHandler(TypedSimpleGraph tsg) {
+			this.tsg = tsg;
+			paperType.add("article");
+			paperType.add("inproceedings");
+			paperType.add("book");
+			paperType.add("proceedings");
+			paperType.add("phdthesis");
+			paperType.add("mastersthesis");
+			paperType.add("incollection");
+			
+			venueType.add("journal");
+			venueType.add("booktitle");
+			
+			currentAuthors = new ArrayList<Author>();
+		}
+
+		private Locator locator;
+
+		private Paper current;
+		private Venue currentVenue;
+		private List<Author> currentAuthors;
+		
+		private String Value;
+		private String key;
+		private String recordTag;
+		
+		int total = 2160375;
+		int perc = 0;
+		int i=0;
+
+		public void setDocumentLocator(Locator locator) {
+			this.locator = locator;
+		}
+
+		public void startElement(String namespaceURI, String localName,
+				String rawName, Attributes atts) throws SAXException {
+
+			if(perc > 5) return;
+			
+			if(paperType.contains(rawName)){				
+				if(perc < i++/(float)total * 100f){
+					perc++;
+					logger.info(perc + "% loaded");
+				}
+				current = new Paper();
+				currentVenue = null;
+				currentAuthors.clear();
+			}else{
+				key = rawName;
+			}
+			Value = "";
+		}
+
+		public void endElement(String namespaceURI, String localName,
+				String rawName) throws SAXException {			
+			if(perc > 5) return;
+			
+			if(paperType.contains(rawName)){
+				tsg.addVertex(current);
+				if(currentVenue == null){
+					currentVenue = new Venue(current.getAttribute("publisher"));					
+				}
+				tsg.addVertex(currentVenue);				
+				tsg.addEdge(current, currentVenue);
+				for(Author a : currentAuthors){
+					tsg.addVertex(a);
+					tsg.addEdge(current, a);
+				}
+			}else if(venueType.contains(rawName)){
+				currentVenue = new Venue(Value);
+			}else if(rawName.equalsIgnoreCase("title")){
+				current.setTitle(Value);
+			}else if(rawName.equalsIgnoreCase("author")){
+				currentAuthors.add(new Author(Value));
+			}else{
+				current.addAttribute(rawName, Value);
+			}
+			
+			
+		}
+
+		public void characters(char[] ch, int start, int length)
+				throws SAXException {
+			if(perc > 5) return;
+			Value += new String(ch, start, length);
+		}
+
+		private void Message(String mode, SAXParseException exception) {
+			System.out.println(mode + " Line: " + exception.getLineNumber()
+					+ " URI: " + exception.getSystemId() + "\n" + " Message: "
+					+ exception.getMessage());
+		}
+
+		public void warning(SAXParseException exception) throws SAXException {
+
+			Message("**Parsing Warning**\n", exception);
+			throw new SAXException("Warning encountered");
+		}
+
+		public void error(SAXParseException exception) throws SAXException {
+
+			Message("**Parsing Error**\n", exception);
+			throw new SAXException("Error encountered");
+		}
+
+		public void fatalError(SAXParseException exception) throws SAXException {
+
+			Message("**Parsing Fatal Error**\n", exception);
+			throw new SAXException("Fatal Error encountered");
+		}
+	}
+
+	public static void main(String[] args) {
+		File data = new File("./data/dblp/dblp.xml.gz");
+
 		TypedSimpleGraph tsg = new TypedSimpleGraph(TypedEdge.class);
 		try {
 			NINALogger.setup();
 			loadDBLPGraphFromFile(FileHandler.toInputStream(data), tsg);
-			PrintStatistics.PrintTypedGraphStatTable(tsg, "./data/dblp/testStats", "DBLPTypedGraph");
+			PrintStatistics.PrintTypedGraphStatTable(tsg,
+					"./data/dblp/testStats", "DBLPTypedGraph");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}

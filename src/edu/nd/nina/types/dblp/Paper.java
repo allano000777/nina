26ec4351a3d87849a1bb56bde4f923b89065ed79
Tim Weninger <weninge1@illinois.edu>
2013-04-23 14:23:30 -0500

    Fixed bug in paper

--- src/edu/nd/nina/types/dblp/Paper.java
+++ src/edu/nd/nina/types/dblp/Paper.java
@@ -37,31 +37,6 @@ public class Paper extends Type {
 	public String toString(){
 		return name + "<Paper>" + index;
 	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((index == null) ? 0 : index.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Paper other = (Paper) obj;
-		if (index == null) {
-			if (other.index != null)
-				return false;
-		} else if (!index.equals(other.index))
-			return false;
-		return true;
-	}
 
 	public void addAttribute(String name, String value) {
 		attributes.put(name, value);
